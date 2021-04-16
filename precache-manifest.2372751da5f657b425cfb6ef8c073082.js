fc248430ff7016e86d1de60298d637d4af38379c
dondeeslafiesta <82632494+dondeeslafiesta@users.noreply.github.com>
2021-04-16 01:09:14 -0400

    initial commit

---
+++ precache-manifest.2372751da5f657b425cfb6ef8c073082.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9bd9bc54aea32dd8116cfd8555c539fd",
+    "url": "/index.html"
+  },
+  {
+    "revision": "82bc0952a29e3a9be973",
+    "url": "/static/css/main.26aa524c.chunk.css"
+  },
+  {
+    "revision": "39e8c4530bdddb408b55",
+    "url": "/static/js/2.61337ec9.chunk.js"
+  },
+  {
+    "revision": "3c73c585782ac05880c0f89bcfdbba5a",
+    "url": "/static/js/2.61337ec9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "82bc0952a29e3a9be973",
+    "url": "/static/js/main.8d37d59e.chunk.js"
+  },
+  {
+    "revision": "2cb30dbdb2fea6fbb1dd",
+    "url": "/static/js/runtime-main.bdda97e8.js"
+  }
+]);
